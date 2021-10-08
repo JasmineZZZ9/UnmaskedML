a919e2cdfc22ad9e49569794a1871660497a4681
GrantPerkins <gcperk20@gmail.com>
2021-10-08 12:36:40 -0400

    Base train method

--- nets/detect/faster_rcnn.py
+++ nets/detect/faster_rcnn.py
@@ -25,3 +25,15 @@ from object_detection.builders import model_builder
 class FasterRCNNTrainer:
     def __init__(self):
         self.model_config = "/model_meta/faster_rcnn_inception_resnet_v2_1024x1024_coco17_tpu-8/pipeline.config"
+
+    def train():
+        model_lib_v2.eval_continuously(
+            pipeline_config_path=FLAGS.pipeline_config_path,
+            model_dir=FLAGS.model_dir,
+            train_steps=FLAGS.num_train_steps,
+            sample_1_of_n_eval_examples=FLAGS.sample_1_of_n_eval_examples,
+            sample_1_of_n_eval_on_train_examples=(
+                FLAGS.sample_1_of_n_eval_on_train_examples),
+            checkpoint_dir=FLAGS.checkpoint_dir,
+            wait_interval=300, timeout=FLAGS.eval_timeout)
+
