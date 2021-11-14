06862d1ae416023af3afb985b414f781e16f3643
Jasmine Zeng <zengmingjie9@gmail.com>
2021-11-14 14:58:54 -0500

    modify Discriminator and create the SN-Patch loss part

--- nets/gan/deepfill.py
+++ nets/gan/deepfill.py
@@ -188,6 +188,82 @@ class Generator(tf.keras.Model):
     #     x = self.layer_12(x)
     #     return x
 
+# Spectral-Normalized
+class SpectralNormalization(tf.keras.layers.Wrapper):
+    """
+    implement for spectral-normalized discriminator
+    for SN-PatchGAN loss
+    """
+    def __init__(self, layer, iteration=1, eps=1e-12, training=True, **kwargs):
+        self.layer = layer
+        self.iteration = iteration
+        self.eps = eps
+        self.do_power_iteration = training
+        if not isinstance(layer, tf.keras.layers.Layer):
+            raise ValueError(
+                'Please initialize `TimeDistributed` layer with a '
+                '`Layer` instance. You passed: {input}'.format(input=layer))
+        super(SpectralNormalization, self).__init__(layer, **kwargs)
+
+    def build(self, input_shape):
+        self.layer.build(input_shape)
+
+        self.w = self.layer.kernel
+        self.w_shape = self.w.shape.as_list()
+
+        self.v = self.add_weight(shape=(1, self.w_shape[0] * self.w_shape[1] * self.w_shape[2]),
+                                 initializer=tf.initializers.TruncatedNormal(stddev=0.02),
+                                 trainable=False,
+                                 name='sn_v',
+                                 dtype=tf.float32)
+
+        self.u = self.add_weight(shape=(1, self.w_shape[-1]),
+                                 initializer=tf.initializers.TruncatedNormal(stddev=0.02),
+                                 trainable=False,
+                                 name='sn_u',
+                                 dtype=tf.float32)
+
+        super(SpectralNormalization, self).build()
+
+    def call(self, inputs):
+        self.update_weights()
+        output = self.layer(inputs)
+        self.restore_weights()  # Restore weights because of this formula "W = W - alpha * W_SN`"
+        return output
+
+    def update_weights(self):
+        w_reshaped = tf.reshape(self.w, [-1, self.w_shape[-1]])
+
+        u_hat = self.u
+        v_hat = self.v  # init v vector
+
+        if self.do_power_iteration:
+            for _ in range(self.iteration):
+                v_ = tf.matmul(u_hat, tf.transpose(w_reshaped))
+                v_hat = v_ / (tf.reduce_sum(v_ ** 2) ** 0.5 + self.eps)
+
+                u_ = tf.matmul(v_hat, w_reshaped)
+                u_hat = u_ / (tf.reduce_sum(u_ ** 2) ** 0.5 + self.eps)
+
+        sigma = tf.matmul(tf.matmul(v_hat, w_reshaped), tf.transpose(u_hat))
+        self.u.assign(u_hat)
+        self.v.assign(v_hat)
+
+        self.layer.kernel.assign(self.w / sigma)
+
+    def restore_weights(self):
+        self.layer.kernel.assign(self.w)
+
+class discriminator_SN(tf.keras.layers.Layer):
+  def __init__(self, filters, size=5, stride=2):
+    super(discriminator_SN, self).__init__(name='')
+    self.sn_conv = SpectralNormalization(
+        tf.keras.layers.Conv2D(filters, size, strides=stride, padding='same',
+                               activation=tf.keras.layers.LeakyReLU(alpha=0.2))
+    )
+
+  def call(self, inputs):
+    return self.sn_conv(inputs)
 
 class Discriminator(tf.keras.Model):
     """
@@ -200,21 +276,22 @@ class Discriminator(tf.keras.Model):
         super(Discriminator, self).__init__()
 
         filter = 64
-        kernel_size = 5
-        stride = 2
+        #kernel_size = 5
+        #stride = 2
 
-        self.layer_1 = ConvolutionalLayer(filter, kernel_size, stride=stride)
-        self.layer_2 = ConvolutionalLayer(2*filter, kernel_size, stride=stride)
-        self.layer_3 = ConvolutionalLayer(4*filter, kernel_size, stride=stride)
-        self.layer_4 = ConvolutionalLayer(4*filter, kernel_size, stride=stride)
-        self.layer_5 = ConvolutionalLayer(4*filter, kernel_size, stride=stride)
-        self.layer_6 = ConvolutionalLayer(4*filter, kernel_size, stride=stride)
+        self.sn_conv_1 = discriminator_SN(filter)
+        self.sn_conv_2 = discriminator_SN(2*filter)
+        self.sn_conv_3 = discriminator_SN(4*filter)
+        self.sn_conv_4 = discriminator_SN(4*filter)
+        self.sn_conv_5 = discriminator_SN(4*filter)
+        self.sn_conv_6 = discriminator_SN(4*filter)
+        self.flatten = tf.keras.layers.Flatten()
 
     def call(self, inputs):
-        x = self.layer_1(inputs)
-        x = self.layer_2(x)
-        x = self.layer_3(x)
-        x = self.layer_4(x)
-        x = self.layer_5(x)
-        x = self.layer_6(x)
-        return x
+        x = self.sn_conv_1(inputs)
+        x = self.sn_conv_2(inputs)
+        x = self.sn_conv_3(inputs)
+        x = self.sn_conv_4(inputs)
+        x = self.sn_conv_5(inputs)
+        x = self.sn_conv_6(inputs)
+        return self.flatten(inputs)
