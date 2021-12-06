e805e51202cd6174e078869b092324d8c9c492fc
GrantPerkins <gcperk20@gmail.com>
2021-12-06 12:34:38 -0500

    Add mask detection to GAN branch

--- nets/detect/faster_rcnn.py
+++ nets/detect/faster_rcnn.py
@@ -1,26 +1,35 @@
-from object_detection import model_lib_v2
-
+import model_lib_v2_test
+import os
 
 # dataset needs to be in TFRecord format
 
 class FasterRCNNTrainer:
     def __init__(self):
         self.model_config = "/unmasked/data/pipeline.config"
-        self.model_dir = "/model_meta/faster_rcnn_inception_resnet_v2_1024x1024_coco17_tpu-8/"
-        self.ckpt_dir = "/model_meta/faster_rcnn_inception_resnet_v2_1024x1024_coco17_tpu-8/checkpoint"
+        self.ckpt_dir = "/model_meta/faster_rcnn_resnet152_v1_640x640_coco17_tpu-8"
 
     def train(self):
         """
         Must change values in config file to be accurate before this method works
         :return:
         """
-        model_lib_v2.eval_continuously(
+        model_lib_v2_test.train_loop(
             pipeline_config_path=self.model_config,
-            model_dir=self.model_dir,
+            model_dir=self.ckpt_dir,
+            train_steps=25000,
+            checkpoint_every_n=200,
+            checkpoint_max_to_keep=250)
+
+    def evaluate(self):
+        model_lib_v2_test.eval_continuously(
+            pipeline_config_path=self.model_config,
+            model_dir=self.ckpt_dir,
             train_steps=500,
-            sample_1_of_n_eval_examples=None,
-            sample_1_of_n_eval_on_train_examples=5,
-            checkpoint_dir=self.ckpt_dir,
+            sample_1_of_n_eval_examples=1,
+            checkpoint_dir=os.path.join(self.ckpt_dir, "point"),
             wait_interval=300, timeout=3600)
 
 
+if __name__ == "__main__":
+    trainer = FasterRCNNTrainer()
+    trainer.train()
